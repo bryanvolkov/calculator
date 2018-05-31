3621e1270915f45f41b99d47975502540f5ef25d
bryanvolkov <bryan_2199@hotmail.com>
2018-05-30 19:48:14 -0700

    More updates

--- src/Calculator.scala
+++ src/Calculator.scala
@@ -8,18 +8,17 @@ object Calculator {
     val tkizer = new Tokenizer
     val rpnconverter = new RPNConverter
     val evaluator = new Evaluator
+    
     print("Enter expression: ")
     var input = scanner.nextLine()
+    
     while(input != ""){
-      input = sanitize(input)
-      if(inputAnalyzer.analyze(input)){
-        var queue:Queue[Token] = tkizer.tokenize(input)
-        queue = rpnconverter.toRPN(queue)
-        var result: Double = evaluator.evaluate(queue)
-        for(i <- queue)
-          print(i.value + " ")
-        println("")
-        println(result)
+      input = sanitize(input) // sanitize the input
+      if(inputAnalyzer.analyze(input)){ // Analyze input
+        var queue:Queue[Token] = tkizer.tokenize(input) // tokenize the input
+        queue = rpnconverter.toRPN(queue) // arrange tokens in RPN
+        printRPN(queue)
+        println(evaluator.evaluate(queue)) // evaluate RPN expression and print it
       }
       else println("Error")
       
@@ -27,6 +26,12 @@ object Calculator {
       input = scanner.nextLine()
     }
     print("End of program...")
+  }
+  
+  def printRPN(queue:Queue[Token]){
+    for(token <- queue)
+      print(token.value.toString() + " ")
+    println("")
   }
   
    def sanitize(str:String): String = {
