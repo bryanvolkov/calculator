280a3bb0eebfabf8b94a872ec7fb76ddad925b5a
bryanvolkov <bryan_2199@hotmail.com>
2018-05-31 22:54:16 -0700

    adding comments

--- src/Calculator.scala
+++ src/Calculator.scala
@@ -2,61 +2,70 @@ import scala.util.control.Breaks._
 import scala.collection.mutable.Queue
 import scala.math._
 
+
 object Calculator {
-  private val NUMBER:Int = 0
+  private val NUMBER:Int = 0 
   private val OPERATOR:Int = 1
+  
+  // main function that runs the whole program
   def main(args: Array[String]){
-    val scanner = new java.util.Scanner(System.in)
-    val inputAnalyzer = new InputAnalyzer
-    val tkizer = new Tokenizer
-    val rpnconverter = new RPNConverter
-    val evaluator = new Evaluator
+    val scanner = new java.util.Scanner(System.in) // scanner to read user's input
+    val inputAnalyzer = new InputAnalyzer // this object is used to analyze user's input
+    val tkizer = new Tokenizer // this object is used to tokenize user's input
+    val rpnconverter = new RPNConverter // this object is used to convert user's input to reverse polish notation
+    val evaluator = new Evaluator // this object is used to evaluate the reverse polish notation which gives the result of the mathematical expression
     
+    // get user's input
     print("Enter expression: ")
     var input = scanner.nextLine()
-    
+
+    // repeat while the user's input is not an empty string
     while(input != ""){
       input = sanitize(input) // sanitize the input
-      if(inputAnalyzer.analyze(input)){ // Analyze input
+      if(inputAnalyzer.analyze(input)){ // Analyze input, and if it's correct then
         var queue:Queue[Token] = tkizer.tokenize(input) // tokenize the input
         queue = rpnconverter.toRPN(queue) // arrange tokens in RPN
-        printRPN(queue)
+        printRPN(queue) // print the reverse polish notation version of the user's input
         println("Res: " + doubleToIntFormat(evaluator.evaluate(queue))) // evaluate RPN expression and print it
       }
-      else println("Error")
+      else println("Error") // if user's input is rejected then print the Error message
       
+      // get user's input
       print("\nEnter expression: ")
       input = scanner.nextLine()
     }
-    print("End of program...")
+    print("End of program...")// this message let's know the user that the program has ended
   }
   
+  // This function prints the contents of a queue, a queue whose contents are in reseverse polish natation order
   def printRPN(queue:Queue[Token]){
     print("RPN: ")
-    for(token <- queue)
-      if(token.vtype == NUMBER)
-        print(doubleToIntFormat(token.value.asInstanceOf[Double]) + " ")
-      else print(token.value.toString() + " ")
+    for(token <- queue) // get token by token from the queue
+      if(token.vtype == NUMBER) // if the token is a number, then
+        print(doubleToIntFormat(token.value.asInstanceOf[Double]) + " ") // print that number in its correct format
+      else print(token.value.toString() + " ") // print the operator
     println("")
   }
   
+  // This function returns the double number in integer format if the decimal part of the double number is 0
   def doubleToIntFormat(num: Double): String = {
-    var d = num - floor(num)
-    var res = ""
-    if(d < 1 && d > 0 || d < 0 && d > -1)
-      res = num.toString()
+    var d = num - floor(num) // calculate the decimal part of the double num
+    var res = "" // back-up string
+    if(d < 1 && d > 0 || d < 0 && d > -1) // if the decimal part is in (0,1) or (-1,0)
+      res = num.toString() // return the double number as a string the way it is
     else{
-      res = floor(num).toString() 
-      res = res.substring(0,res.length-2)
+      res = floor(num).toString() // get the whole number of the double number and make it as a string 
+      res = res.substring(0,res.length-2) // remove the ".0" part of the string
     }
-    res
+    res // return the bakc-up string
   }
   
+  // This function removes the spaces from string str
    def sanitize(str:String): String = {
-    var res:String = ""
-    for(i <- 0 until str.length)
-      if(str.charAt(i) != ' ')
-        res += str.charAt(i)
-    res
+    var res:String = "" // back up string to return
+    for(i <- 0 until str.length) // loop through the string
+      if(str.charAt(i) != ' ')// if the character of the string at position i is not a space, then
+        res += str.charAt(i) // put that character in the back up string
+    res // return the back up string
   }
 }
--- src/InputAnalyzer.scala
+++ src/InputAnalyzer.scala
@@ -1,52 +1,60 @@
 import scala.util.control.Breaks._
 
-// The only function of this class is to accept or reject a mathematical expression
+// The only function of this class is to accept or reject a mathematical expression.
 // that has any of the following characters:
 // 0 1 3 4 5 6 7 8 9
 // + - * /
 // ( )
 // .
+// This function is a Push Down Automata
 
 class InputAnalyzer {
-  private val q0 = 0
-  private val q1 = 1
-  private val q2 = 2
-  private val q3 = 3
-  private val q4 = 4
-  private val q5 = 5
-  private val q6 = 6
-  private val q7 = 7
-  private val q8 = 8
+  private val q0 = 0 // state 0
+  private val q1 = 1 // state 1
+  private val q2 = 2 // state 2
+  private val q3 = 3 // state 3
+  private val q4 = 4 // state 4
+  private val q5 = 5 // state 5
+  private val q6 = 6 // state 6
+  private val q7 = 7 // state 7
+  private val q8 = 8 // state 8
   
+  // This is an array of functions
   private val check_state: Array[(Char) => Boolean] = Array(check_q0, check_q1, check_q2, check_q3, check_q4, check_q5, check_q6, check_q7, check_q8)
   
+  // This stack is used only to make sure that the opening parentheses of the mathematical expression have a matching closing parentheses
   private var stk:Stack[Char] = new Stack[Char]
   
+  // this function returns true if the character is a number
   private def is_number(c:Char):Boolean = {
      '0' <= c && c <= '9'
   }
   
+  // this function returns true if the character is an operator
   private def is_operator(c:Char):Boolean = {
     c == '-' || c == '+' || c == '/' || c == '*'
   }
   
-  private var state = q0
+  private var state = q0 // starting state of the push down automaton is State 0
   
+  // This function analyzes a mathematical expression
   def analyze(str:String):Boolean = {
-    state = q0
-    stk.push('$')
-    var accept:Boolean = true
-    breakable{
-    for(i <- 0 until str.length)
-      if(!check_state(state)(str.charAt(i))){
-        accept = false
-        break}
+    state = q0// go back to the initial state
+    stk.push('$') // end of stack
+    var accept:Boolean = true // variable whose content will be returned
+    breakable{ // breakable statement to be able to break the enclosed for loop 
+    for(i <- 0 until str.length)// while the string has characters to be read
+      if(!check_state(state)(str.charAt(i))){ // check the current state
+        accept = false // do not accept the string
+        break} // break for loop
     }
+    // if string is to be accepted, then check if the last state that was checked is a final state
     if(accept) accept = state == q2 || state == q4 || state == q8
     stk.empty_except(1)
-    accept
+    accept // return true if the string was accepted
   }
   
+  // check state 0
   private def check_q0(c:Char):Boolean = {
      if (is_number(c)) state = q2
      else if(c == '.') state = q3
@@ -58,6 +66,7 @@ class InputAnalyzer {
     true
    }
    
+  // check state 1
    private def check_q1(c:Char): Boolean = {
      if(is_number(c)) state = q2
     else if(c == '.') state = q3
@@ -68,6 +77,7 @@ class InputAnalyzer {
     true
     }
    
+     // check state 2
    private def check_q2(c:Char): Boolean = {
     if(is_number(c)) state = q2
     else if(c == '.') state = q3
@@ -79,12 +89,14 @@ class InputAnalyzer {
     true
    }
    
+     // check state 3
     private def check_q3(c:Char): Boolean = {
       if(is_number(c)) state = q4
       else return false
       true
     }
       
+    //   // check state 4
      private def check_q4(c:Char): Boolean = {
     if(is_number(c)) state = q4
     else if(is_operator(c)) state = q5
@@ -95,6 +107,8 @@ class InputAnalyzer {
     true
     }
      
+     
+       // check state 5
     private def check_q5(c:Char): Boolean = {
     if(is_number(c)) state = q2
     else if(c == '.') state = q3
@@ -105,7 +119,7 @@ class InputAnalyzer {
     else return false
     true
   }
-    
+      // check state 6
    private def check_q6(c:Char): Boolean = {
     if(is_number(c)) state = q2
     else if(c == '.') state = q3
@@ -117,6 +131,7 @@ class InputAnalyzer {
     true
     }
    
+     // check state 7
     private def check_q7(c:Char): Boolean = {
     if(is_number(c)) state = q2
     else if(c == '.') state = q3
@@ -128,6 +143,7 @@ class InputAnalyzer {
     true 
     }
     
+      // check state 8
    private def check_q8(c:Char): Boolean = {
     if(is_operator(c)) state = q5
     else if(c == ')'){
